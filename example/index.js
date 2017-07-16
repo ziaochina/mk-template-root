389ff461c9bccb418aaed8b1ef16199a83e302bd
ziaochina <ziaochina@gmail.com>
2017-07-16 12:53:48 +0800

    zlj

---
+++ example/index.js
@@ -0,0 +1,25 @@
+import { config, start, componentFactory } from 'mk-meta-engine'
+import myConfig  from './config'
+
+import mk_template_root_about from './apps/mk-template-root/apps/xr-template-root-about/index.js'
+import mk_template_root_helloWorld from './apps/mk-template-root/apps/xr-template-root-helloWorld/index.js'
+import mk_template_root from './apps/mk-template-root/index.js'
+
+const apps = {
+	[mk_template_root_about.name]:mk_template_root_about,	
+	[mk_template_root_helloWorld.name]:mk_template_root_helloWorld,	
+	[mk_template_root.name]:mk_template_root,	
+}
+
+
+config(myConfig({apps}))
+
+
+import * as mkComponents from 'mk-component'
+
+Object.keys(mkComponents).forEach(key=>{
+	componentFactory.registerComponent(key, mkComponents[key])
+})
+	
+
+start()
